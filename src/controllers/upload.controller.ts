8aaea332f714bd094e7249ecdea915f4f1991717
minhhquann1508 <126867340+minhhquann1508@users.noreply.github.com>
2025-08-01 22:05:08 +0700

    controller/upload

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -8,6 +8,22 @@ import { CONST } from '../utils/constant';
 import { CustomRequest } from '../types/request';
 import uploadImage from '../helper/uploadFile';
 import { CloudinaryUploadResult } from '../types/cloudinaryResponse';
+import Course from '../models/course.model';
+
+export const uploadImageUrl = asyncHandler(
+  async (req: Request, res: Response) => {
+    const file = req.file;
+
+    if (!file)
+      throw new CustomError(http.BAD_REQUEST, 'Không có file được tải lên');
+
+    const result = (await uploadImage(file.buffer)) as CloudinaryUploadResult;
+    if (!result)
+      throw new CustomError(http.BAD_REQUEST, 'Upload ảnh không thành công');
+
+    res.json({ message: 'Upload avatar thành công', url: result.secure_url });
+  },
+);
 
 export const uploadAvatar = asyncHandler(
   async (req: CustomRequest, res: Response) => {
@@ -33,5 +49,31 @@ export const uploadAvatar = asyncHandler(
     await userData.save();
 
     res.json({ message: 'Upload avatar thành công', user: userData });
+  },
+);
+
+export const uploadCourseThumbnail = asyncHandler(
+  async (req: CustomRequest, res: Response) => {
+    const { slug } = req.params;
+    const { user } = req;
+    if (!slug) throw new CustomError(http.BAD_REQUEST, 'Không tìm thấy slug');
+
+    if (!user) throw new CustomError(http.UNAUTHORIZED, 'Bạn chưa đăng nhập');
+
+    const file = req.file;
+    if (!file) throw new CustomError(http.BAD_REQUEST, 'Không tìm thấy file');
+
+    const course = await Course.findOne({ slug, createdBy: user.id });
+    if (!course)
+      throw new CustomError(http.BAD_REQUEST, 'Không tìm thấy khóa học');
+
+    const result = (await uploadImage(file.buffer)) as CloudinaryUploadResult;
+    if (!result)
+      throw new CustomError(http.BAD_REQUEST, 'Upload ảnh không thành công');
+
+    course.thumbnail = result.secure_url;
+    await course.save();
+
+    res.json({ message: 'Upload avatar thành công', course });
   },
 );
--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -2,10 +2,17 @@ import expess from 'express';
 import upload from '../middlewares/upload';
 import { checkPermission } from '../middlewares/checkRole';
 import { verifyToken } from '../middlewares/verifyToken';
-import { uploadAvatar } from '../controllers/upload.controller';
+import { uploadAvatar, uploadImageUrl } from '../controllers/upload.controller';
 
 const router = expess.Router();
 
-router.post('/testing', upload.single('file'), verifyToken, uploadAvatar);
+router
+  .post('/', upload.single('file'), verifyToken, uploadImageUrl)
+  .post('/avatar', upload.single('file'), verifyToken, uploadAvatar)
+  .post(
+    '/course-thumbanil/:slug',
+    [upload.single('file'), verifyToken, checkPermission('instructor')],
+    uploadAvatar,
+  );
 
 export default router;
